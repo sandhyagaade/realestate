2794485485d2ee2037f0e430cad54b7a015a8779
sandhyagaade <gadesandhyarani9@gmail.com>
2024-12-26 13:22:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+ $(document).ready(function() {
+$(".down").click(function() {
+     $('html, body').animate({
+         scrollTop: $(".up").offset().top
+     }, 1500);
+ });
+});
+
+$(document).ready(function() {
+$(".up").click(function() {
+     $('html, body').animate({
+         scrollTop: $(".down").offset().top
+     }, 1000);
+ });
+});
